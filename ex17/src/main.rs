29ba015b3fd54e06311c65f27ecd79298c1a10dd
YawKar <yawkarpov@gmail.com>
2023-04-22 19:54:19 +0300

    excs: add 17th exercise

---
+++ ex17/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    println!("To run tests for the ex. 17 use: cargo test -p ex17");
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+#[cfg_attr(test, derive(Debug, PartialEq))]
+enum Color {
+    /* Place here serde attribute to add next aliases: "Chili red", "Rust" */
+    Red,
+    /* Place here serde attribute to add next aliases: "Azure", "Aero" */
+    Blue,
+    /* Place here serde attribute to add next aliases: "Lime", "Harlequin" */
+    Green,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Color;
+
+    #[test]
+    fn deserialize_different_shades_of_red() {
+        let shades = vec![r#""Chili red""#, r#""Rust""#];
+        for shade  in shades {
+            assert_eq!(Color::Red, serde_json::from_str(shade).unwrap());
+        }
+    }
+    
+    #[test]
+    fn deserialize_different_shades_of_blue() {
+        let shades = vec![r#""Azure""#, r#""Aero""#];
+        for shade in shades {
+            assert_eq!(Color::Blue, serde_json::from_str(shade).unwrap());
+        }
+    }
+
+    #[test]
+    fn deserialize_different_shades_of_green() {
+        let shades = vec![r#""Lime""#, r#""Harlequin""#];
+        for shade in shades {
+            assert_eq!(Color::Green, serde_json::from_str(shade).unwrap());
+        }
+    }
+}
